a256d09594f5a4ac6ea3d4b1f7280288985ed725
answerfan <Spawn333>
2015-10-30 15:37:18 +0600

    24+

---
+++ CodedUITestProject1/CodedUITestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью указанного ниже 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("CodedUITestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodedUITestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// При установке для ComVisible значения false типы в этой сборке станут невидимыми 
+// для компонентов COM. Если необходимо получить доступ к типу в сборке из 
+// COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID указан для идентификатора typelib, если этот проект предоставляется COM
+[assembly: Guid("fde5128e-4a50-49c0-9474-9486a41d5867")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер сборки
+//      Номер редакции
+//
+// Можно указать все значения или установить сборку и номера редакций по умолчанию 
+// с помощью символа "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
